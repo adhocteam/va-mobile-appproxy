c1caf29e53843ff4b234240357ce4b667fe7e05e
Ben Shyong <hello@benshyong.com>
2020-05-28 10:23:17 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const url = require('url');
+
+const PORT = process.env.PORT || 3000;
+const DEEP_LINK = 'theva://home';
+
+function redirect(response, url) {
+  response.writeHead(302, {
+    Location: url,
+  });
+  response.end();
+}
+
+http
+  .createServer((request, response) => {
+    // get parameters from request
+    const parameters = url.parse(request.url, true).query;
+
+    // if parameters contain authServiceUrl, this request comes from the application
+    if (parameters.authServiceUrl) {
+      // redirect user to the authUrl
+      redirect(response, decodeURIComponent(parameters.authServiceUrl));
+      return;
+    }
+
+    // redirect response from the auth service to your application
+    redirect(response, DEEP_LINK);
+  })
+  .listen(PORT);
